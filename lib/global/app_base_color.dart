2d05e727c40c4883755049446e2038e9e157f59f
nhq.dev <nhq.dev.it@gmail.com>
2022-10-13 22:21:25 +0700

    no message

--- lib/global/app_base_color.dart
+++ lib/global/app_base_color.dart
@@ -2,6 +2,7 @@ import 'package:dttv/base/base_controller.dart';
 
 abstract class BaseColor {
   Color get randomColor;
+  List<Color> get rdGradientColor;
   Color get appBaseColor;
   Color get baseColor;
   Color get grey0;
